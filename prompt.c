1eeff80e0333b2f55cdf2a4d34d7e2b23a74eb61
lew <barpas@zoho.com>
2014-10-19 10:23:37 +0200

    present mp3/ogg files before asking what to do whith it.

--- prompt.c
+++ prompt.c
@@ -1,23 +1,49 @@
 #include "prompt.h"
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+static void present_file( const char *path );
+static bool simple_fork();
+
 static const char *file_prompt =
 "What to do with %s?\nsh -c '<your string>' <filepath> > ";
 void prompt_file( const char *path ){
+    present_file( path );
+
     char command[255];
     printf( file_prompt, path );
     fgets( command, 255, stdin );
 
+    if( simple_fork() )
+        execl( "/bin/sh", "sh", "-c", command, path, (char*)NULL );
+}
+
+static void present_file( const char *path ){
+    const char *command = NULL;
+    const char *extension = path + strlen(path) - 4;
+
+    if( strcmp( extension, ".mp3" ) == 0
+        || strcmp( extension, ".ogg" ) == 0 )
+        command = "/usr/bin/mplayer";
+
+    if( command ){
+        if( simple_fork() )
+            execl( command, command, path, (char*)NULL );
+    }
+}
+
+static bool simple_fork(){
     pid_t child_pid = fork();
     if( child_pid < 0 )
         printf( "%s:%u: fork failed", __FILE__, __LINE__ );
-    else if( child_pid == 0 )
-        execl( "/bin/sh", "sh", "-c", command, path, (char*)NULL );
-    else
+    else if ( child_pid != 0 )
         waitpid( child_pid, NULL, 0 );
-}
+    else return true;
 
+    return false;
+}
