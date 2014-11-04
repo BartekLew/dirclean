f810a4d373b9b32d2f26079e7142f6957af6672c
lew <barpas@zoho.com>
2014-11-04 22:45:11 +0100

    remove pointless function

--- dirclean.c
+++ dirclean.c
@@ -4,9 +4,7 @@
 #include "prompt.h"
 
 static void user_need_reread_usage( const char *ran_path );
-static void print_error( struct big_picture *work );
 static void handle_directory( struct big_picture *work );
-static void handle_node( struct big_picture *work );
 
 int main( int arguments_count, char **arguments ){
     if( arguments_count != 2 )
@@ -14,21 +12,10 @@ int main( int arguments_count, char **arguments ){
     
     struct big_picture *work = 
         grow_big_picture( NULL, arguments[1] );
-    handle_node( work );
+    handle_directory( work );
     free( work );
 
     return 0;
-}
-
-static void handle_node( struct big_picture *work ){
-    at_fs_location(
-        work, &prompt_file,
-        &handle_directory, &print_error
-    );
-}
-
-static void print_error( struct big_picture *work ){
-    fprintf( stderr, "Error on %s.\n", work->subject );
 }
 
 static void handle_directory( struct big_picture *work ){
