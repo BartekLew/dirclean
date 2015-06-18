4221afc5acbf7c997d5f924c29fc4a78a39b156f
lev <barpas@zoho.com>
2015-06-18 21:02:51 +0200

    allow attachment of javascript file

--- lsphoto.c
+++ lsphoto.c
@@ -4,19 +4,28 @@
 #include "big_picture.h"
 #include "fs.h"
 
+static char *attached_js = NULL;
+
 static struct big_picture *make_galery( struct big_picture* );
 void html_for_image( struct big_picture*, void *user_data );
 
 int main( int arguments_count, char **arguments ){
     if( arguments_count == 1 ){
         fprintf( stderr, "[lsphoto] no argument given.\n"
-            "lsphoto dir - make html galery in directory\n" );
+            "lsphoto dir [javascript] - make html galery in directory\n"
+            "optionaly js script file [javascript] can be given to be attached\n"
+        );
         return 1;
     }
 
-    free_work( make_galery( &(struct big_picture){
+    if( arguments_count == 3 )
+        attached_js = arguments[2];
+
+    free_work( make_galery(
+        &(struct big_picture){
             .subject = arguments[1]
-    } ) );
+        } 
+    ) );
 
     return 0;
 }
@@ -39,6 +48,12 @@ make_galery( struct big_picture *location ){
             &make_galery
         ), (void *) output
     );
+
+    if( attached_js )
+        fprintf( output,
+            "    <script src=\"%s\" type=\"text/javascript\"></script>\n",
+            attached_js
+        );
 
     fprintf( output, "  </body>\n</html>\n" );
 
