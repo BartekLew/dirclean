67c5740188f8523c26a2ac7737caa98923ac51b3
lev <barpas@zoho.com>
2015-06-23 20:23:28 +0200

    fix dir changing bug

--- fs.c
+++ fs.c
@@ -86,7 +86,7 @@ struct big_picture *without_directories(
 
     while( cursor->next ){
         struct stat info;
-        if( stat( work->subject, &info ) == 0 
+        if( stat( cursor->subject, &info ) == 0 
             && S_ISDIR( info.st_mode )
         ){
             directory_action( cursor->next );
